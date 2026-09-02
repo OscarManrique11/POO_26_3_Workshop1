f3767b4a6e916acb7525834715c37d7e4acced02
oscar manrique <ofmanriquea@udistrital.edu.co>
2026-09-01 19:21:17 -0500

    Implementar mayorDeTresNumeros y suma

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -7,16 +7,11 @@ public class Workshop {
 
     // Método que suma dos números enteros
     public int sumarDosNumeros(int a, int b) {
-        // TODO: Implementar el método para retornar la suma de dos números enteros.
-        // Ejemplo: Si a = 3 y b = 5, el resultado debería ser 8.
         return a+b;
-	//return 0;
     }
 
     // Método que encuentra el mayor de tres números enteros
     public int mayorDeTresNumeros(int a, int b, int c) {
-        // TODO: Implementar el método para retornar el mayor de los tres números enteros.
-        // Ejemplo: Si a = 3, b = 7, y c = 5, el resultado debería ser 7.
       if (a>= b && a >=c) return a;
     if (b>=c) return b;  
       return c;
