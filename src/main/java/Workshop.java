4473a10b141dc71fd4a50a4c99701def55817748
oscar manrique <ofmanriquea@udistrital.edu.co>
2026-09-08 18:06:21 -0500

    esPalindromo hecho

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -231,9 +231,9 @@ public class Workshop {
 
     // Método que verifica si una cadena es un palíndromo
     public boolean esPalindromo(String cadena) {
-        // TODO: Implementar el método para verificar si una cadena es un palíndromo.
-        // Ejemplo: Si cadena = "madam", el resultado debería ser true.
-        return false;
+        cadena = cadena.toLowerCase().replace(" ", "");
+        String invertida = new StringBuilder(cadena).reverse().toString();
+        return cadena.equals(invertida);
     }
 
     // Método que cuenta el número de palabras en una cadena
