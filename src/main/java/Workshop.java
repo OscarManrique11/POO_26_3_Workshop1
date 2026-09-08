0cbd2a0709cf8604f3f02df49dd9628f4937f56b
oscar manrique <ofmanriquea@udistrital.edu.co>
2026-09-08 17:14:28 -0500

    ordenarArreglo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -136,9 +136,9 @@ public class Workshop {
 
     // Método que ordena un arreglo en orden ascendente
     public int[] ordenarArreglo(int[] arreglo) {
-        // TODO: Implementar el método para ordenar un arreglo en orden ascendente.
-        // Ejemplo: Si arreglo = [5, 4, 3, 2, 1], el resultado debería ser [1, 2, 3, 4, 5].
-        return new int[0];
+        int[] ordenado = arreglo.clone();
+        java.util.Arrays.sort(ordenado);
+        return ordenado;
     }
 
     // Método que elimina los duplicados de un arreglo
