acc92d5a75d685feb2a40410edc3fc99367d00e0
oscar manrique <ofmanriquea@udistrital.edu.co>
2026-09-08 18:59:06 -0500

    areaCirculo hecho

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -338,7 +338,7 @@ Rock crushes Scissors
     }
 
     public double areaCirculo(double radio) {
-        return 0.0;
+        return Math.PI * radio * radio;
     }
 
     public String zoodiac(int day, int month) {
